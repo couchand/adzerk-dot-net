8754419a1eb52ad348f295b729696cefa1800048
Andrew Couch <acouch@stackexchange.com>
2014-12-30 11:46:22 -0800

    sometimes end date is null anyway

--- Adzerk.Api/Models/Flight.cs
+++ Adzerk.Api/Models/Flight.cs
@@ -180,7 +180,7 @@ namespace Adzerk.Api.Models
             f.Name = Name;
 
             f.StartDate = DateTime.Parse(StartDate);
-            if (!NoEndDate)
+            if (!NoEndDate && f.EndDate != null)
             {
                 f.EndDate = DateTime.Parse(EndDate);
             }
