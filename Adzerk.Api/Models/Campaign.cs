ade318b97589cf555f6b610fae675dee19e1e958
Andrew Couch <acouch@stackexchange.com>
2015-01-13 12:38:55 -0800

    campaign EndDate nullable

--- Adzerk.Api/Models/Campaign.cs
+++ Adzerk.Api/Models/Campaign.cs
@@ -16,7 +16,7 @@ namespace Adzerk.Api.Models
         public bool IsActive { get; set; }
         public decimal Price { get; set; }
         public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
+        public DateTime? EndDate { get; set; }
         public IEnumerable<Flight> Flights { get; set; }
     }
 
