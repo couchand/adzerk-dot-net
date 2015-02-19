55a09f50e1afac6c0bdff559290974bcfc733ede
Andrew Couch <acouch@stackoverflow.com>
2015-02-19 13:06:47 -0800

    update geotargeting

--- Adzerk.Api/Models/GeoTargeting.cs
+++ Adzerk.Api/Models/GeoTargeting.cs
@@ -8,9 +8,10 @@ namespace Adzerk.Api.Models
 {
     public class GeoTargeting
     {
+        public long LocationId { get; set; }
         public string CountryCode { get; set; }
         public string Region { get; set; }
-        public string MetroCode { get; set; }
+        public int MetroCode { get; set; }
         public bool IsExclude { get; set; }
     }
 }
