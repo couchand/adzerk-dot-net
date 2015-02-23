dc987f8d9679e27d743ca3683713b369019dcd7b
Andrew Couch <acouch@stackoverflow.com>
2015-02-23 15:56:27 -0800

    model for previous

---
+++ Adzerk.Api/Models/Creative.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Adzerk.Api.Models
+{
+    public class Creative
+    {
+        long AdvertiserId { get; set; }
+        long AdTypeId { get; set; }
+
+        string Body { get; set; }
+        string Url { get; set; }
+        string Title { get; set; }
+        string Alt { get; set; }
+
+        bool IsSync { get; set; }
+        bool IsDeleted { get; set; }
+        bool IsActive { get; set; }
+
+        bool IsHTMLJS { get; set; }
+        string ScriptBody { get; set; }
+    }
+}
